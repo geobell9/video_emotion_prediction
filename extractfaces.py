e30f370614e483710095e1c44b52704942492c5e
Bandara <dbandara@univ.fairfield.local>
2020-10-25 17:51:15 -0400

    adding script for extracting cropped faces from video

---
+++ extractfaces.py
@@ -0,0 +1,72 @@
+import numpy as np
+from cv2 import cv2
+import os.path
+import boundingbox 
+from pathlib import Path
+
+VIDEO_NUMBER = 105
+BOUNDING_BOX_PATH = ".\\bounding-boxes"
+ANNOTATIONS_PATH =  ".\\annotations"
+
+def main():
+    #load annotations
+    valence_annotations = boundingbox.load_annotations(ANNOTATIONS_PATH +"\\valence\\"+ str(VIDEO_NUMBER)+".txt")
+    arousal_annotations = boundingbox.load_annotations(ANNOTATIONS_PATH +"\\arousal\\"+ str(VIDEO_NUMBER)+".txt")
+
+    frame_number = 0
+    cap = cv2.VideoCapture(".\\videos\\"+str(VIDEO_NUMBER)+".avi")
+    while(cap.isOpened()):
+        frame_number += 1
+
+        #load bounding box coords
+        bounding_box = boundingbox.load_points(BOUNDING_BOX_PATH +"\\"+ str(VIDEO_NUMBER)+"\\"+str(frame_number)+".pts")
+        if not bounding_box:
+            continue
+        frame = cap.read()[1]       
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #write bounding box on image
+        pts = np.array([[bounding_box[0][0],bounding_box[0][1]],[bounding_box[1][0],bounding_box[1][1]],[bounding_box[2][0],bounding_box[2][1]],[bounding_box[3][0],bounding_box[3][1]]], np.int32)
+        pts = pts.reshape((-1,1,2))
+        img = cv2.polylines(gray,[pts],True,(0,255,255))
+        crop_img = img[ int(bounding_box[0][1]):int(bounding_box[1][1]),int(bounding_box[0][0]):int(bounding_box[2][0]),]
+        cv2.imshow("cropped", crop_img)
+        valence_value = float(valence_annotations[frame_number])
+        arousal_value = float(arousal_annotations[frame_number])
+        #save crop to path based on valence value
+        if valence_value <= -1 and valence_value < -0.5:
+            Path(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\valence\\low\\").mkdir(parents=True, exist_ok=True)#create directory path if it doesnt exist
+            cv2.imwrite(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\valence\\low\\"+str(frame_number)+".png",crop_img)
+        elif valence_value <= -0.5 and valence_value < 0.5:
+            Path(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\valence\\neutral\\").mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\valence\\neutral\\"+str(frame_number)+".png",crop_img)
+        elif valence_value <= 0.5 and valence_value <= 1:
+            Path(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\valence\\high\\").mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\valence\\high\\"+str(frame_number)+".png",crop_img)
+        else:
+            print("error writing valence image crop")
+        #save crop to path based on arousal value
+        if arousal_value <= -1 and arousal_value < -0.5:
+            Path(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\arousal\\low\\").mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\arousal\\low\\"+str(frame_number)+".png",crop_img)
+        elif arousal_value <= -0.5 and arousal_value < 0.5:
+            Path(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\arousal\\neutral\\").mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\arousal\\neutral\\"+str(frame_number)+".png",crop_img)
+        elif arousal_value <= 0.5 and arousal_value <= 1:
+            Path(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\arousal\\high\\").mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(".\\faces"+"\\"+str(VIDEO_NUMBER)+"\\arousal\\high\\"+str(frame_number)+".png",crop_img)
+        else:
+            print("error writing arousal image crop")
+     
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+if __name__ == '__main__':
+    main()
